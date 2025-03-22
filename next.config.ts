1c64c89775a941acf37262e55728ad0b45e35c79
irsad-najib <irsad.putra270305@gmail.com>
2025-03-22 13:27:52 +0700

    fix: allow images from Azure Blob Storage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blobstorageeqariah.blob.core.windows.net",
+        pathname: "/eqariah-images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
