e3cccb39b1472615dc2d9958557d4abcd5440d9e
irsad-najib <irsad.putra270305@gmail.com>
2025-03-23 10:10:27 +0700

    fix: another error

--- src/app/component/API.tsx
+++ src/app/component/API.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://BEeqariah-endpoint-d5akg9g9csfcfqa2.z02.azurefd.net";
+const API_URL = "https://api.eqariah.com";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
